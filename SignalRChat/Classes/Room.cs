59ff615031d0cdcf53065db483043592d08fba35
Brian Soetens <brian40@live.nl>
2019-02-14 12:08:39 +0100

    Added Room class and started lobby scaffold

---
+++ SignalRChat/Classes/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Classes
+{
+    public class Room
+    {
+        public string RoomName { get; set; }
+        public virtual ICollection<User> UsersInRoom { get; set; }
+    }
+}
